1418296c8c75d831a459f1e748d7306ce4f37560
TassioSense <tassio.jordao@sensenova.com.br>
2019-03-21 19:03:16 -0300

    aualizando barra de carregamento + tempo estimado de carregamento

--- aplicativo/public/src/controller/DropBoxController.js
+++ aplicativo/public/src/controller/DropBoxController.js
@@ -4,7 +4,12 @@ class DropBoxController {
 
         this.btnSendFileEl = document.querySelector("#btn-send-file"); 
         this.btnInputFileEl = document.querySelector("#files"); 
-        this.snackModalEl = document.querySelector("#react-snackbar-root"); 
+        this.snackModalEl = document.querySelector("#react-snackbar-root");
+        this.progressBarEl = this.snackModalEl.querySelector(".mc-progress-bar-fg")
+        this.nameFileEl = this.snackModalEl.querySelector(".filename");
+        this.timeLeftEl = this.snackModalEl.querySelector(".timeleft");
+        this.startUploadTime = 0;
+
 
         this.initEvents();
     }
@@ -20,11 +25,19 @@ class DropBoxController {
             this.uploadTask(event.target.files); //pegando os arquivos selecionados
             
             //exibir barra de carregamento de imagem via CSS
-            this.snackModalEl.style.display = "block";
+            this.modalShow();
+            
+            this.btnInputFileEl.value = ""; //zerando a informação do arquivo
 
         })
 
 
+    }
+
+    modalShow (show = true) {
+
+        //exibir barra de carregamento de imagem via CSS
+        this.snackModalEl.style.display = (show) ? "block" : "none";
     }
 
     uploadTask(files) {
@@ -49,6 +62,8 @@ class DropBoxController {
 
                 ajax.onload = event => {
 
+                    this.modalShow(false);
+
                     try {
 
                         //ao fazer o carregamento, precisamos passar as informações via JSON
@@ -64,26 +79,95 @@ class DropBoxController {
                 }
 
                 ajax.onerror = event => {
+                    
                     reject(event);
+                    this.modalShow(false);
+                
                 }
+
+                /*Método AJAX.upload.onprogress
+                retorna em tempos e tempos as informações do arquivo que está sendo carregado
+                */
+
+                ajax.upload.onprogress = event => {
+                    //console.log(event)
+                    this.uploadProgress(event, file);
+                };
+
+
                 /*Para ler o arquivo e envia-lo pelo ajax, precisamos ler esse arquivo.
                 Vamos usar a API FormData pra isso
                  */
-
                 let formData = new FormData ();
                 //depois vamos inserir cada file dentro do formData usando append. Note que há dois atributos em append
                 //append("nomeQueApareceNoPOST", arquivo)
                 formData.append("input-file", file)
 
+                this.startUploadTime = Date.now(); //registro da hora que foi pedido o carregamento do arquivo
+
                 ajax.send(formData);//enviando o formData com os files
 
-            }))
+            }));
 
         });
 
         return Promise.all(promises)
            
         
+    }
+
+    uploadProgress(event, file) {
+        /*Quando fazemos o upload.onprogress, passamos os dados do evento (quantidade de bytes) para a variável bytes
+        delas usamos as informações a cada progresso que o navegador retorna pra gente */
+
+        let timeSpent = (Date.now() - this.startUploadTime); //tempo gasto
+        let loaded = event.loaded; //quanto carregado do arquivo até o momento em bytes
+        let total = event.total; //total de bytes do arquivo
+        let porcent = parseInt((loaded/total) * 100);
+        let timeLeft = ((100 - porcent) * timeSpent) / porcent;
+        /*calculo:
+            PORCENTAGEM                 TIME
+              porcent                  timeSpent
+            100 - porcent                  x = tempo que resta
+        */
+
+        //vamos passa a informação para o CSS, de quanto carregado temos
+        this.progressBarEl.style.width = porcent+"%"; 
+
+        //filename and time
+        this.nameFileEl.innerHTML = file.name;
+        this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeLeft);
+
+        //console.log(timeSpent, porcent); //truque para sabermos quanto equivalo uma quantidade relativa em porcentagem do carregamento do arquivo e sua respectiva quantidade em bytes
+    }
+
+    formatTimeToHuman(duration) {
+
+        let seconds = parseInt((duration /1000) % 60); //convertendo em segundo e tirando o módulo
+        let minutes = parseInt((duration /(1000 * 60)) % 60); //convertendo em minutos e tirando o modulo
+        let hours = parseInt((duration /(1000 * 60 * 60)) % 24);//convertendo em horas
+
+        if (hours > 0) {
+
+            return `${hours}h:${minutes}min:${seconds}s`;
+
+        }
+
+        
+        if (minutes > 0) {
+
+            return `${minutes}min:${seconds}s`;
+
+        }
+
+        
+        if (seconds > 0) {
+
+            return `${seconds}s`;
+
+        }
+
+        return "";
     }
 
 
--- aplicativo/routes/index.js
+++ aplicativo/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+/*Vamos utilizar Formidable module
+Modulo para carregar arquivos de formulários e upload
+*/
+var formidable = require("formidable");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload', (req, res) => {
+
+  /*vamos usar o IncomindForm para chamar o formulário
+  dentro do método temos que passar o diretório que será salvo os dados e também pedir para keep Extension (para preservar a extensão e conseguirmos abrir)
+  */
+  let form = new formidable.IncomingForm({
+      uploadDir: "./upload",
+      keepExtensions: true,
+
+  });
+
+  /*Com os dados do formulário carregados, 
+  precisamos interpreta-los (parse) para que o formidable saiba o que é campo (field) e o que é arquivo (files) */
+  form.parse(req, (err, fields, files) => {
+    
+     res.json({
+       files: files //passando os arquivos para um objeto
+
+     }); //pegando os dados
+  
+  });
+ 
+});
+
 
 module.exports = router;
